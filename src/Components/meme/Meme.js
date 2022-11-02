e73b84b31b7036f3e0942d43d05a57f1e5f384b2
Karitas <karitashall@gmail.com>
2022-11-02 07:56:34 +0000

    publish

--- src/Components/meme/Meme.js
+++ src/Components/meme/Meme.js
@@ -11,30 +11,32 @@ import {
 } from "./styles";
 
 export default function Meme() {
+  // State that handles our list of memes
+  // Starts out empty ([]) will get filled by API
+  const [allMemes, setAllMemes] = useState([]);
+
+  // State that handles the meme the user makes
+  // It starts out with a default picture and no text
   const [meme, setMeme] = useState({
     topText: "",
     bottomText: "",
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
 
-  const [allMemes, setAllMemes] = useState([]);
-
+  // "Fetching" meme images from the API
+  // Parsing the API results into a javascript object (data)
+  // Storing them in the setAllMemes array
   useEffect(() => {
     fetch("https://api.imgflip.com/get_memes")
       .then((res) => res.json())
       .then((data) => setAllMemes(data.data.memes));
   }, []);
 
-  function getMemeImage() {
-    const randomNumber = Math.floor(Math.random() * allMemes.length);
-    const url = allMemes[randomNumber].url;
-    setMeme((prevMeme) => ({
-      ...prevMeme,
-      randomImage: url,
-    }));
-  }
-
-  /*const getMemeImage = useCallback(() => {
+  /* useCallback will create a function that gets updated whenever a
+   variable in the dependency array changes.
+   This function grabs a random image out of the allMemes array and
+   places it in the meme object */
+  const getMemeImage = useCallback(() => {
     const randomNumber = Math.floor(Math.random() * allMemes.length);
     const url = allMemes[randomNumber].url;
     setMeme((prevMeme) => {
@@ -43,9 +45,15 @@ export default function Meme() {
         randomImage: url,
       };
     });
-  }, []);*/
+    // Dependency array
+  }, [allMemes]);
 
-  function handleChange(event) {
+  // Handles user input and updates the meme object
+  const handleChange = useCallback((event) => {
+    /* "name" is a prop for the FormInput and has 
+    the value "topText" or "bottomText. This matches the properties
+    on the meme object
+    */
     const { name, value } = event.target;
     setMeme((prevMeme) => {
       return {
@@ -53,25 +61,24 @@ export default function Meme() {
         [name]: value,
       };
     });
-  }
+  }, []);
 
-  // const style = {
-  //   backgroundImage: "url(" + meme.randomImage + ")",
-  // };
-
+  /* Here I am using styled components and they can be 
+  viewed in their own styles.js files
+  */
   return (
     <Main>
       <Form>
         <FormInput
           type="text"
-          placeholder="top-text"
+          placeholder="Top text"
           name="topText"
           value={meme.topText}
           onChange={handleChange}
         ></FormInput>
         <FormInput
           type="text"
-          placeholder="bottom-text"
+          placeholder="Bottom text"
           name="bottomText"
           value={meme.bottomText}
           onChange={handleChange}
